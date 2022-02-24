eb895291f11f80f5d847ed8402fccebdd056c0c4
Matthias Wolff <matthias4331@gmail.com>
2022-02-24 23:43:12 +0100

    implemented full cycle with pedestrian lights

--- src/Ampelsteuerung.js
+++ src/Ampelsteuerung.js
@@ -1,65 +1,91 @@
-import { Component } from 'react';
+import { Component } from "react";
 import TFGAmpel from "./TFGAmpel";
 
 class Ampelsteuerung extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            Ampeln:["A", "A", "A", "A", "A"],
-            active:0
-         }
+        this.state = {
+            Ampeln: ["Auto", "Fußgänger", "Auto", "Auto", "Fußgänger", "Auto", "Auto", "Fußgänger"],
+            active: -1,
+            pedestrian: false
+        };
     }
 
-    next(callerId){
-        if (callerId !== this.state.active){
+    next(callerId) {
+        if (callerId !== this.state.active) {
             return;
         }
-        console.log("called")
         let nextIndex;
-        let length = this.state.Ampeln.length
-        for(let i = this.state.active; i < length; i++){
-            if(this.state.Ampeln[i] === "A" && this.state.active !== i){
+        if (this.state.pedestrian) this.setState({ pedestrian: false, active: -1 });
+        for (let i = this.state.active; i < this.state.Ampeln.length; i++) {
+            if (this.state.Ampeln[i] === "Auto" && this.state.active !== i) {
                 nextIndex = i;
                 break;
             }
-            
         }
-        if(nextIndex === undefined){
-            nextIndex = 0;
+        if (nextIndex === undefined) {
+            this.setState({ pedestrian: true });
+            for (let i = 0; i < this.state.Ampeln.length; i++) {
+                if (this.state.Ampeln[i] === "Fußgänger" && this.state.active !== i) {
+                    nextIndex = i;
+                    break;
+                }
+            }
         }
-        
-        this.setState({active:nextIndex})
+
+        this.setState({ active: nextIndex });
     }
-    componentDidUpdate(){
-        if(this.state.active > this.state.Ampeln.length - 1){
-            this.setState({active:0})
+
+    componentDidMount() {
+        this.next(-1);
+    }
+
+    componentDidUpdate() {
+        if (this.state.active > this.state.Ampeln.length - 1) {
+            for (let i = 0; i < this.state.Ampeln.length; i++) {
+                if (this.state.Ampeln[i] === "Auto" && this.state.active !== i) {
+                    this.setState({ active: i });
+                    break;
+                }
+            }
         }
     }
+
     render() {
-        console.log("render")
-        console.log(this.state.active) 
-        return ( 
+        return (
             <div>
-            {this.state.Ampeln.map((Ampel, index)=>{
-                return <TFGAmpel key = {index} id = {index} active = {this.state.active === index} next = {this.next.bind(this)}/>
-            })}
-                <button onClick = {() => {
-                    let newAmpeln = this.state.Ampeln.slice();
-                    newAmpeln.push("A");
-                    this.setState({Ampeln:newAmpeln})
-                }} className = "button">
+                {this.state.Ampeln.map((Ampel, index) => {
+                    return (
+                        <TFGAmpel
+                            key={index}
+                            id={index}
+                            type={Ampel}
+                            active={this.state.active === index || (Ampel === "Fußgänger" && this.state.pedestrian)}
+                            next={this.next.bind(this)}
+                        />
+                    );
+                })}
+                <button
+                    onClick={() => {
+                        let newAmpeln = this.state.Ampeln.slice();
+                        newAmpeln.push("Auto");
+                        this.setState({ Ampeln: newAmpeln });
+                    }}
+                    className="button">
                     add
                 </button>
-                <button onClick = {() => {
-                    let newAmpeln = this.state.Ampeln.slice();
-                    newAmpeln.pop();
-                    this.setState({Ampeln:newAmpeln})
-                }} className = "button">
+                <button
+                    onClick={() => {
+                        let newAmpeln = this.state.Ampeln.slice();
+                        newAmpeln.pop();
+                        this.setState({ Ampeln: newAmpeln });
+                    }}
+                    className="button">
                     unadd
                 </button>
             </div>
-         );
+        );
     }
 }
- 
+
 export default Ampelsteuerung;
--- src/TFGAmpel.js
+++ src/TFGAmpel.js
@@ -1,58 +1,76 @@
-import { Component } from 'react';
-import './ampel.css';
+import { Component } from "react";
+import "./ampel.css";
 
 class TFGAmpel extends Component {
     constructor(props) {
         super(props);
+        this.timeout = [];
         this.state = {
-            currentState: 0, 
-             stateList : {
-                 0:[true, false, false],
-                 1:[true, true, false],
-                 2:[false, false, true],
-                 3:[false, true, false]
-                }
-        }
+            currentState: 0,
+            stateList: [
+                {
+                    0: [true, false, false],
+                    1: [true, true, false],
+                    2: [false, false, true],
+                    3: [false, true, false]
+                },
+                { 0: [true, false, false], 1: [true, false, false], 2: [false, false, true], 3: [true, false, false] }
+            ]
+        };
     }
-    switchState (stateid) {
-        switch(stateid){
+
+    switchState(stateid) {
+        switch (stateid) {
             case 0:
-                this.setState({currentState: 1});
-                setTimeout(this.switchState.bind(this), 1000, 1);
+                this.setState({ currentState: 1 });
+                this.timeout = setTimeout(this.switchState.bind(this), 1000, 1);
                 break;
             case 1:
-                this.setState({currentState: 2});
-                setTimeout(this.switchState.bind(this), 1000, 2);
+                this.setState({ currentState: 2 });
+                setTimeout(this.switchState.bind(this), 2000, 2);
                 break;
             case 2:
-                this.setState({currentState: 3});
+                this.setState({ currentState: 3 });
                 setTimeout(this.switchState.bind(this), 1000, 3);
                 break;
             case 3:
                 this.props.next(this.props.id);
-                this.setState({currentState: 0});
+                this.setState({ currentState: 0 });
                 break;
         }
     }
-    componentDidMount(){
-        if(this.props.active){
+
+    componentDidMount() {
+        if (this.props.active) {
             setTimeout(this.switchState.bind(this), 1000, 0);
         }
     }
-    componentDidUpdate(){
-        if(this.props.active && this.state.currentState === 0){
+
+    componentDidUpdate() {
+        if (this.props.active && this.state.currentState === 0) {
             setTimeout(this.switchState.bind(this), 1000, 0);
         }
     }
-    render() { 
-        return ( 
-            <span className = 'ampel-wrapper'>
-                <div className = {(this.state.stateList[`${this.state.currentState}`][0] ? '' : 'grey') + ' red light'}></div>
-                <div className = {(this.state.stateList[`${this.state.currentState}`][1] ? '' : 'grey') + ' orange light'}></div>
-                <div className = {(this.state.stateList[`${this.state.currentState}`][2] ? '' : 'grey') + ' green light'}></div>
-            </span>
-         );
+
+    render() {
+        if (this.props.type === "Auto") {
+            return (
+                <span className="ampel-wrapper">
+                    <div className={(this.state.stateList[0][`${this.state.currentState}`][0] ? "" : "grey") + " red light"}></div>
+                    <div className={(this.state.stateList[0][`${this.state.currentState}`][1] ? "" : "grey") + " orange light"}></div>
+                    <div className={(this.state.stateList[0][`${this.state.currentState}`][2] ? "" : "grey") + " green light"}></div>
+                </span>
+            );
+        } else if (this.props.type === "Fußgänger") {
+            return (
+                <span className="ampel-wrapper">
+                    <div className={(this.state.stateList[1][`${this.state.currentState}`][0] ? "" : "grey") + " red light"}></div>
+                    <div className={"light"}></div>
+                    <div className={(this.state.stateList[1][`${this.state.currentState}`][2] ? "" : "grey") + " green light"}></div>
+                </span>
+            );
+        }
     }
 }
- 
+
 export default TFGAmpel;
